8933c22ed466da2fc48cebf6a57e2fd757d6eb16
anisssal <anis.alathiif@gmail.com>
2017-12-07 20:18:42 +0700

    ini tambah java action

--- app/src/main/java/com/blogspot/programer27android/haripertamaiak/MainActivity.java
+++ app/src/main/java/com/blogspot/programer27android/haripertamaiak/MainActivity.java
@@ -1,9 +1,8 @@
 package com.blogspot.programer27android.haripertamaiak;
 
 import android.content.Intent;
-import android.support.v4.text.TextUtilsCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -38,5 +37,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+       password.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               System.out.print("test");
+           }
+       });
     }
+
+
 }
